e7926c4e71cb45dbbb79cf34832bfaa955931359
yo-ga91 <magikal@mail.ru>
2016-11-26 17:20:39 +0700

    26.11

--- 2diodAndButton.js
+++ 2diodAndButton.js
@@ -1,6 +1,7 @@
-var http= require("http");
+var http= require("http").createServer(handler);    //on req - hand
+var io=require("socket.io").listen(http);    //socket.io for permanent connection between server and client    
 var firmata = require("firmata");
-
+var fs =require("fs");  //variable for file system for providing html
 
 console.log("Starting the code");
 var board = new firmata.Board("/dev/ttyACM0", function()
@@ -11,63 +12,42 @@ var board = new firmata.Board("/dev/ttyACM0", function()
     console.log("Activete of Pin 13");
     board.pinMode(13, board.MODES.OUTPUT);  //!pin 8 as out
 });
-
-var io=require("soket.io").listen(http);    //socket.io for permanent connection between server and client    
-
-io.sockets.on("connection", function(socket) {
-    socket.on("commandToArduino", function(commandNo){
-        if (commandNo == "1") {
+function handler(req,res) 
+{
+    fs.readFile(__dirname+ "/2diodAndButtons.html",
+    function (err, data)
+        {
+            if(err)
+                {
+                    res.writeHead(500,{"Content-Type":"text/plain"});
+                    return res.end("Error loading html page");
+                }
+            res.writeHead(200);
+            res.end(data);
+        })
+}       
+http.listen(8080);   //listen on port 8080 
+io.sockets.on("connection", function(socket) 
+{
+    console.log("commandToArduino");
+    socket.on("commandToArduino", function(commandNo)
+    {
+        if (commandNo == "1") 
+        {
             board.digitalWrite(13, board.HIGH); // write HIGH on pin 13
         }
-        if (commandNo == 0) {
+        if (commandNo == 0) 
+        {
             board.digitalWrite(13, board.LOW); // write LOW on pin 13
         }
+        if (commandNo == "1") 
+        {
+            board.digitalWrite(8, board.HIGH); // write HIGH on pin 13
+        }
+        if (commandNo == 0) 
+        {
+            board.digitalWrite(8, board.LOW); // write LOW on pin 13
+        }
+
     });
 });
-
-http.createServer(function(req,res) 
-{
-
-    var socket = io.connect("172.16.22.118:8080"); // create socket - connect to it
-    
-    function on () 
-    {
-        socket.emit("commandToArduino", 1);
-    }
-
-    function off () 
-    {
-        socket.emit("commandToArduino", 0);
-    } 
-    
-    
-    var parts=req.url.split("/"),           //split request on / character
-    operator1 = parseInt(parts[1],10),       //10 is radex - decimal notation
-    operator2 = parseInt(parts[2],10);       //10 is radex - decimal notation
-
-    if(operator1==0)
-    {
-        console.log("Putting LED to OFF.");
-        board.digitalWrite(13, board.LOW);
-    }
-    if (operator1==1)
-    {
-        console.log("Putting LED to ON");
-        board.digitalWrite(13, board.HIGH);
-    }
-    if(operator2==0)
-    {
-        console.log("Putting LED to OFF.");
-        board.digitalWrite(8, board.LOW);
-    }
-    if (operator2==1)
-    {
-        console.log("Putting LED to ON");
-        board.digitalWrite(8, board.HIGH);
-//    }
-    res.writeHead(200,{"Content-Type":"text/plain"});    //200=OK
-    res.write("For test write into broweser 123.1.2.3:8080/1 \n");
-    res.end("The value of the operator is:"+operator1);
-//    res.end("The value of the operator is:"+operator2);
-    }
-}).listen(8080, "172.16.22.118");   //listen on port 8080
---
+++ Examp12.js
@@ -0,0 +1,32 @@
+var board = new firmata.Board("/dev/ttyACM0", function() { // ACM Abstract Control Model for serial communication with Arduino (could be USB)
+    console.log("Priključitev na Arduino");
+    board.pinMode(2, board.MODES.OUTPUT); // direction of DC motor
+    board.pinMode(3, board.MODES.PWM); // PWM of motor i.e. speed of rotation
+    board.pinMode(4, board.MODES.OUTPUT); // direction DC motor
+    board.digitalWrite(2,1); // initialization of digital pin 2 to rotate Left on start
+    board.digitalWrite(4,0); // initialization of digital pin 2 to rotate Left on start
+});
+
+socket.on("sendPWM", function(pwm){
+        board.analogWrite(3,pwm);
+
+        socket.emit("messageToClient", "PWM set to: " + pwm);        
+    });
+    
+    socket.on("left", function(value){
+        board.digitalWrite(2,value.AIN1);
+        board.digitalWrite(4,value.AIN2);
+        socket.emit("messageToClient", "Direction: left");
+    });
+    
+    socket.on("right", function(value){
+        board.digitalWrite(2,value.AIN1);
+        board.digitalWrite(4,value.AIN2);
+        socket.emit("messageToClient", "Direction: right");
+    });
+    
+   socket.on("stop", function(value){
+        board.analogWrite(3,value);
+        socket.emit("messageToClient", "STOP");
+    });
+
